03a3bafc198de0514bfcc47c83540d0e1352909f
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-03-17 18:17:33 -0300

    Requisito 7 resolução

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ function createNewItem() {
   const newTask = document.createElement('li');
   const list = document.querySelector('#lista-tarefas');
   newTask.innerHTML = textoTarefa.value;
+  newTask.addEventListener('click', corFundo);
+  function corFundo() {
+    newTask.style.backgroundColor = 'rgb(128,128,128)';
+  }
   list.appendChild(newTask);
 }
 
