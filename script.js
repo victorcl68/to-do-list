a7aaa2d601f34b01a970fa3236da67d83af71d74
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-03-17 19:29:10 -0300

    Requisito 8 resolução

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ function createNewItem() {
   const newTask = document.createElement('li');
   const list = document.querySelector('#lista-tarefas');
   newTask.innerHTML = textoTarefa.value;
-  newTask.addEventListener('click', corFundo);
   function corFundo() {
-    newTask.style.backgroundColor = 'rgb(128,128,128)';
+    const cont = document.querySelectorAll('.selected');
+    if (cont.length === 0) {
+      newTask.className = 'selected';
+      newTask.style.backgroundColor = 'rgb(128,128,128)';
+    } else {
+      const ant = document.querySelector('.selected');
+      ant.className = '';
+      ant.style.backgroundColor = 'white';
+      newTask.className = 'selected';
+      newTask.style.backgroundColor = 'rgb(128,128,128)';
+    }
   }
+  newTask.addEventListener('click', corFundo);
   list.appendChild(newTask);
 }
 
