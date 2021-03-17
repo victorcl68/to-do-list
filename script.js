4296f7a4be95a110fac94a6b8d5e68a23975aa5f
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-03-17 18:07:39 -0300

    Requisito 5 resolução

---
+++ script.js
@@ -0,0 +1,16 @@
+const textoTarefa = document.querySelector('#texto-tarefa');
+
+function createNewItem() {
+  const newTask = document.createElement('li');
+  const list = document.querySelector('#lista-tarefas');
+  newTask.innerHTML = textoTarefa.value;
+  list.appendChild(newTask);
+}
+
+function eraseItem() {
+  textoTarefa.value = '';
+}
+
+const theButton = document.querySelector('#criar-tarefa');
+theButton.addEventListener('click', createNewItem);
+theButton.addEventListener('click', eraseItem);
