a947314a3841285a5f2691a4fdfa83fd041e8e7e
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-03-18 09:16:09 -0300

    Requisito 9, terceira tentativa de resolução

--- script.js
+++ script.js
@@ -5,31 +5,36 @@ function createNewItem() {
   const newTask = document.createElement('li');
   const list = document.querySelector('#lista-tarefas');
   newTask.innerHTML = textoTarefa.value;
-  function lineOut() {
-    newTask.style.textDecoration = 'none';
-    newTask.classList.remove('risked');
-  }
-  function riskItem() {
-    newTask.style.textDecoration = 'line-through';
-    newTask.className += ' risked';
-    newTask.addEventListener('dblclick', lineOut);
-  }
+  // eslint-disable-next-line max-lines-per-function
   function corFundo() {
     const cont = document.querySelectorAll('.selected');
     if (cont.length === 0) {
-      newTask.className = 'selected';
+      newTask.className += ' selected';
       newTask.style.backgroundColor = 'rgb(128,128,128)';
     } else {
       const ant = document.querySelector('.selected');
       ant.classList.remove('selected');
       ant.style.backgroundColor = 'white';
-      newTask.className = 'selected';
+      newTask.className += ' selected';
       newTask.style.backgroundColor = 'rgb(128,128,128)';
     }
   }
+  function riskItemOrNot() {
+    const conta = document.querySelectorAll('.completed');
+    if (conta.length === 0) {
+      newTask.className += ' completed';
+      newTask.style.textDecoration = 'line-through solid rgb(0, 0, 0)';
+      newTask.addEventListener('dblclick', riskItemOrNot);
+    } else {
+      const anta = document.querySelector('.completed');
+      anta.classList.remove('completed');
+      newTask.style.textDecoration = '';
+    }
+  }
   newTask.addEventListener('click', corFundo);
-  newTask.addEventListener('dblclick', riskItem);
+  newTask.addEventListener('dblclick', riskItemOrNot);
   list.appendChild(newTask);
+  newTask.className += 'created';
 }
 
 function eraseItem() {
